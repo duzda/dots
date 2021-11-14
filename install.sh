012360c12c2e6bd4d6b4b778341459564af07651
duzda <duzda666@gmail.com>
2021-11-14 10:06:09 +0100

    better file structure

--- install.sh
+++ install.sh
@@ -3,35 +3,20 @@
 read -p "Quick Update (Y/N) should be only used by my lazy ass: " quick
 if [[ $quick == [yY] || $quick == [yY][eE][sS] ]]
 then
-    cp -uv ./.zshrc ~/.config/
-    cp -ruv ./alacritty ~/.config/
-    cp -ruv ./bspwm ~/.config/
-    cp -ruv ./polybar ~/.config/
-    cp -ruv ./redshift ~/.config/
-    cp -ruv ./rofi ~/.config/
-    cp -ruv ./scripts ~/.config/
-    cp -ruv ./gtk-theme ~/.themes/Dracula-pink-accent/
+    cp -uv ./.zshrc ~/
+    cp -ruv ./.config ~/
+    cp -ruv ./.themes ~/
     exit 0
 fi
 
 read -p "Use backup (Y/N): " backup
 if [[ $backup == [yY] || $backup == [yY][eE][sS] ]]
 then
-    cp --backup=simple -i ./.zshrc ~/.config/
-    cp --backup=simple -ir ./alacritty ~/.alacritty/
-    cp --backup=simple -ir ./bspwm ~/.config/
-    cp --backup=simple -ir ./polybar ~/.config/
-    cp --backup=simple -ir ./redshift ~/.config/
-    cp --backup=simple -ir ./rofi ~/.config/
-    cp --backup=simple -ir ./scripts ~/.config/
-    cp --backup=simple -ir ./gtk-theme ~/.themes/Dracula-pink-accent/
+    cp --backup=simple -i ./.zshrc ~/
+    cp --backup=simple -ir ./.config ~/
+    cp --backup=simple -ir ./.themes ~/
 else
-    cp -i ./.zshrc ~/.config/
-    cp -ir ./alacritty ~/.config/
-    cp -ir ./bspwm ~/.config/
-    cp -ir ./polybar ~/.config/
-    cp -ir ./redshift ~/.config/
-    cp -ir ./rofi ~/.config/
-    cp -ir ./scripts ~/.config/
-    cp -ir ./gtk-theme ~/.themes/Dracula-pink-accent/
+    cp -i ./.zshrc ~/
+    cp -ir ./.config ~/
+    cp -ir ./.themes ~/
 fi
